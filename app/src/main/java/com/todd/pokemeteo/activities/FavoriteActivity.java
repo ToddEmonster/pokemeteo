81b52f1f5a643b71751e9f92f5e9d24955e70a1e
Todd Raharivelo <t.raharivelo@outlook.fr>
2022-07-19 11:19:48 +0200

    feat: add recycler view

--- app/src/main/java/com/todd/pokemeteo/activities/FavoriteActivity.java
+++ app/src/main/java/com/todd/pokemeteo/activities/FavoriteActivity.java
@@ -1,28 +1,30 @@
-package com.todd.pokemeteo;
+package com.todd.pokemeteo.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.util.Log;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.todd.pokemeteo.R;
 import com.todd.pokemeteo.databinding.ActivityFavoriteBinding;
+import com.todd.pokemeteo.models.City;
+
+import java.util.ArrayList;
 
 public class FavoriteActivity extends AppCompatActivity {
     private static final String TAG = "Favorite Activity";
 
     private ActivityFavoriteBinding binding;
-    private TextView mTextViewMessageContent;
+    private RecyclerView mRecyclerView;
+    private ArrayList<City> mCities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,19 +40,38 @@ public class FavoriteActivity extends AppCompatActivity {
         CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
 //        toolBarLayout.setTitle(getTitle());
 
+        // Return button
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
+        // Floating Action Button
         FloatingActionButton fab = binding.fab;
         fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show());
 
-        mTextViewMessageContent = findViewById(R.id.text_view_message_content);
-        mTextViewMessageContent.setText(extras.getString(Util.EXTRA_MESSAGE));
+        // Recycler View
+        mRecyclerView = findViewById(R.id.favorite_recycler_view);
+        // Items list
+        mCities = new ArrayList<>();
+
+        City city1 = new City("Antananarivo", "Nuageux", "17°C", R.drawable.morpheo_cloud);
+        City city2 = new City("Barcelone", "Ensoleillé", "28°C", R.drawable.morpheo_sun);
+        City city3 = new City("Chongqing", "Légères pluies", "24°C", R.drawable.nenupiot_drizzle);
+        City city4 = new City("New Delhi", "Pluies modérées", "29°C", R.drawable.morpheo_rain);
+
+        mCities.add(city1);
+        mCities.add(city2);
+        mCities.add(city3);
+        mCities.add(city4);
     }
 
+    /**
+     * Methode appelée automatiquement lors du clic sur le bouton Retour dans la toolbar
+     * @param item  l'item cliqué
+     * @return un booleen
+     */
     public boolean onOptionsItemSelected(MenuItem item){
         Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(myIntent);
