703554d22bbcf1eeb506e8000c43edf2f91ad43d
Todd Raharivelo <t.raharivelo@outlook.fr>
2022-07-21 11:42:17 +0200

    feat: fetch Api data and log it

--- app/src/main/java/com/todd/pokemeteo/activities/MainActivity.java
+++ app/src/main/java/com/todd/pokemeteo/activities/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -17,11 +16,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.todd.pokemeteo.R;
+import com.todd.pokemeteo.utils.ApiUtil;
 import com.todd.pokemeteo.utils.Util;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Main Activity";
+    private OkHttpClient mOkHttpClient;
+
     private RelativeLayout mRelativeLayoutConnection;
     private TextView mTextViewNoConnection;
 
@@ -32,7 +42,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate()");
         setContentView(R.layout.activity_main);
+
+        mOkHttpClient = new OkHttpClient();
 
         mRelativeLayoutConnection = findViewById(R.id.relative_layout_connection);
         mTextViewNoConnection = findViewById(R.id.text_view_no_connection);
@@ -51,7 +64,8 @@ public class MainActivity extends AppCompatActivity {
             mButtonFavoris.setOnClickListener(this::onClickFavoris);
 
             mTextViewCityLabel.setText(getString(R.string.city_name));
-            Toast.makeText(this, mTextViewCityLabel.getText(), Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this, mTextViewCityLabel.getText(), Toast.LENGTH_SHORT).show();
+            sendApiCall();
         } else {
             mRelativeLayoutConnection.setVisibility(View.INVISIBLE);
             mTextViewNoConnection.setVisibility(View.VISIBLE);
@@ -67,6 +81,30 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onResume()");
     }
 
+    public void sendApiCall() {
+        Log.d(TAG, "sendApiCall()");
+        String testUrl = String.format(
+                "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s",
+                ApiUtil.testLat, ApiUtil.testLon, ApiUtil.APP_API_KEY
+        );
+        Request request = new Request.Builder().url(testUrl).build();
+        mOkHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d(TAG, "sendApiCall.onResponse()");
+                if (response.isSuccessful()) {
+                    final String stringJson = response.body().string();
+                    Log.d(TAG, stringJson);
+                }
+            }
+        });
+    }
+    
     public void onClickFavoris(View v) {
         Toast.makeText(this, "Clic sur favoris", Toast.LENGTH_SHORT).show();
 
---
+++ app/src/main/java/com/todd/pokemeteo/utils/ApiUtil.java
@@ -0,0 +1,9 @@
+package com.todd.pokemeteo.utils;
+
+public class ApiUtil {
+    public static final String APP_API_KEY = "01897e497239c8aff78d9b8538fb24ea";
+
+    public static final double testLat = -18.901593;
+    public static final double testLon = 47.517997;
+
+}
